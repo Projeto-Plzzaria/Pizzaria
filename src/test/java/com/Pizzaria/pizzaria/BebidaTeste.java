cb433d09d4e5d8687c9d40b8819f49e5bcc8e67e
dogogh <quadros1919@gmail.com>
2023-09-20 16:34:54 -0300

    bebidas teste

--- src/test/java/com/Pizzaria/pizzaria/BebidaTeste.java
+++ src/test/java/com/Pizzaria/pizzaria/BebidaTeste.java
@@ -7,6 +7,7 @@ import com.Pizzaria.pizzaria.Entity.TamanhoB;
 import com.Pizzaria.pizzaria.Repository.BebidasRepository;
 import com.Pizzaria.pizzaria.Service.BebidaService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import static org.mockito.Mockito.when;
 
@@ -23,8 +24,13 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -54,7 +60,6 @@ public class BebidaTeste {
     @Test
     public void testLista() throws Exception {
         when(bebidaService.listartudo()).thenReturn(Collections.emptyList());
-
         mockMvc.perform(get("/api/Bebida/lista")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -63,8 +68,6 @@ public class BebidaTeste {
     @Test
     public void testCadastrarSuccess() throws Exception {
         BebidaDTO bebidaDTO = new BebidaDTO();
-
-
         when(bebidaService.cadastrar(any(Bebida.class)))
                 .thenReturn(new Bebida());
 
@@ -91,6 +94,45 @@ public class BebidaTeste {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.tamanho").value("_1L"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.sabor").value("Cola"));
+    }
+
+    @Test
+    public void testListaAtivo() {
+        boolean ativo = true;
+        List<Bebida> bebidasAtivas = new ArrayList<>();
+        when(bebidasRepository.findByAtivo(ativo)).thenReturn(bebidasAtivas);
+        ResponseEntity<List<BebidaDTO>> response = bebidaController.listaAtivo(ativo);
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(bebidasAtivas.size(), response.getBody().size());
+    }
+
+    @Test
+    public void testDeleteExistente() {
+        Long id = 1L;
+
+        when(bebidasRepository.findById(id)).thenReturn(Optional.of(new Bebida()));
+
+        ResponseEntity<String> response = bebidaController.delete(id);
+
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals("Apagado com sucesso", response.getBody());
+
+        verify(bebidasRepository, times(1)).deleteById(id);
+    }
+
+    @Test
+    public void testAtualizarComSucesso() {
+
+        Long id = 1L;
+
+        when(bebidaService.atualizar(eq(id), any())).thenReturn(new Bebida());
+
+        BebidaDTO bebidaDTO = new BebidaDTO();
+
+        ResponseEntity<?> response = bebidaController.atualizar(id, bebidaDTO);
+
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals("Atualizado com sucesso!", response.getBody());
     }
 
     private String asJsonString(Object obj) {
