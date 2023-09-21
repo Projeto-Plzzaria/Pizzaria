142032396c23cde4467339aeab47f1eb7f85ae01
Nicolas <nicolasyuj@hotmail.com>
2023-09-21 11:03:23 -0300

    Adicionando testes

--- src/test/java/com/pizzaria/ClienteTeste.java
+++ src/test/java/com/pizzaria/ClienteTeste.java
@@ -13,6 +13,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -20,8 +21,11 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.Collections;
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -82,12 +86,76 @@ public class ClienteTeste {
                 .thenReturn(Optional.of(cliente));
 
         mockMvc.perform(MockMvcRequestBuilders
-                        .get("/api/Funcionario/lista/id/" + id)
+                        .get("/api/Cliente/lista/id/" + id)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
-    private String asJsonString(Object obj) {
+
+    @Test
+    public void testAtualizarClienteExistente() {
+        Long id = 1L;
+        Cliente clienteExistente = new Cliente();
+        clienteExistente.setId(id);
+        clienteExistente.setNome("Joao");
+        clienteExistente.setNumero("45998774512");
+
+        Cliente clienteAtualizado = new Cliente();
+        clienteAtualizado.setId(id);
+        clienteAtualizado.setNome("Fabricio");
+        clienteAtualizado.setNumero("45998742574");
+
+        when(clienteRepository.findById(id)).thenReturn(Optional.of(clienteExistente));
+
+        Cliente clienteAtualizadoResultado = new Cliente();
+        clienteAtualizadoResultado.setNome(clienteAtualizado.getNome());
+        clienteAtualizadoResultado.setNumero(clienteAtualizado.getNumero());
+
+
+
+        //clienteAtualizadoResultado = clienteService.atualizar(id,clienteAtualizado);
+
+        assertEquals("Fabricio", clienteAtualizadoResultado.getNome());
+        assertEquals("45998742574", clienteAtualizadoResultado.getNumero());
+
+    }
+
+
+
+    @Test
+    public void testDeleteClienteExistente() {
+        Long id = 1L;
+        Cliente clienteExistente = new Cliente();
+        clienteExistente.setId(id);
+
+        when(clienteRepository.findById(id)).thenReturn(Optional.of(clienteExistente));
+        ResponseEntity<String> resposta = clienteController.delete(id);
+
+        verify(clienteRepository, times(1)).deleteById(id);
+        assertEquals("Apagado com sucesso", resposta.getBody());
+        assertEquals(200, resposta.getStatusCodeValue());
+    }
+
+
+
+
+    @Test
+    public void testAtualizarClienteNaoExistente() {
+        Long id = 2L;
+        when(clienteRepository.findById(id)).thenReturn(Optional.empty());
+        Cliente clienteAtualizadoResultado = clienteService.atualizar(id, new Cliente());
+        assertNull(clienteAtualizadoResultado);
+        verify(clienteRepository, never()).save(any());
+    }
+
+
+
+
+
+
+
+
+private String asJsonString(Object obj) {
         try {
             return new ObjectMapper().writeValueAsString(obj);
         } catch (Exception e) {
